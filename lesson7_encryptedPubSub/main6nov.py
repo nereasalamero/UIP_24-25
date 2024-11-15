425812affeb228bc0f01793b7ee66f97202d7786
Nerea Salamero <nereasalamero@gmail.com>
2024-11-15 10:20:48 +0200

    Charts and Tables
    - Lesson 7 isn't working
    - Lesson 8 is working just fine

--- lesson7_encryptedPubSub/main6nov.py
+++ lesson7_encryptedPubSub/main6nov.py
@@ -9,64 +9,48 @@ class Message():
         self.text = text
         self.passkey = passkey
         self.encrypted_text = encrypt(self.text, self.passkey)
+        self.decrypted_text = decrypt(self.encrypted_text, self.passkey)
 
 def main(page: ft.Page):
     page.title = "Flet Chat"
     messages = ft.Column()
     user = ft.TextField(label="Username", width=150)
-    message = ft.TextField(label="Message", expand=True, on_submit=lambda e: send_click)
-    passkey = ft.TextField(label="Passkey", width=150, password=True)
-    
+    message = ft.TextField(label="Message", expand=True)
+    passkey = ft.TextField(label="Passkey", width=150)
     publish_to = ft.Dropdown(
         label="Publish to",
         options=[
             ft.dropdown.Option("Group 1"),
             ft.dropdown.Option("Group 2"),
             ft.dropdown.Option("Group 3")
-        ],
-        width=200
-    )
+        ])
     subscribe_to = ft.Dropdown(
         label="Subscribe to",
         options=[
             ft.dropdown.Option("Group 1"),
             ft.dropdown.Option("Group 2"),
             ft.dropdown.Option("Group 3")
-        ],
-        width=200,
-        #on_change=lambda e: subscribe_topic(e)
-    )
+        ])
     
     # subscribe to broadcast messages
-    def on_message(msg: Message, e):
-        print(f"Messages: {messages.controls}")
+    def on_message(msg: Message):
         decrypted = decrypt(msg.encrypted_text, passkey.value)
         messages.controls.append(ft.Text(f"{msg.user}: {decrypted}"))
-        print(f"Received message from {msg.user}: {msg.text} // {decrypted}")
         page.update()
 
-    def subscribe_topic(e):
-        topic = subscribe_to.value
-        # page.pubsub.subscribe_topic(topic, on_message)
-        print(f"Subscribed to {topic}")
-        print(f"Messages: {messages.controls}")
-        page.update()
-
-    page.pubsub.subscribe(on_message)
+    page.pubsub.subscribe_topic(subscribe_to.value, on_message)
+    #page.pubsub.subscribe(on_message)
 
     def send_click(e):
         new_msg = Message(user.value, message.value, passkey.value)
-        publish = publish_to.value
-        #page.pubsub.send_all_on_topic(publish, new_msg)
-        print(f"Sent message to {publish}")
-        print(f"Message: {new_msg.text}")
-        page.pubsub.send_all(new_msg)
+        page.pubsub.send_all_on_topic(publish_to.value, new_msg)
+        # page.pubsub.send_all(new_msg)
         # clean up the form
         message.value = ""
         page.update()
     
     send = ft.ElevatedButton("Send", on_click=send_click)
-    #subscribe = ft.ElevatedButton("Subscribe", on_click=subscribe_topic)
+
     page.add(
         ft.Column([
             # Add the messages column to the page
@@ -74,10 +58,8 @@ def main(page: ft.Page):
             # Add the dropdowns and the passkey field to the page
             ft.Row(controls=[publish_to, subscribe_to, passkey]),        
             # Add the user input fields and the send button to the page
-            ft.Row(controls=[user, message, send]),
-            # ft.Row(controls=[subscribe])
+            ft.Row(controls=[user, message, send])
         ])
     )
-    page.update()
 
 ft.app(main, view=ft.AppView.WEB_BROWSER)
--- lesson8_chartTables/main.py
+++ lesson8_chartTables/main.py
@@ -2,7 +2,63 @@ import flet as ft
 
 
 def main(page: ft.Page):
-    page.add(ft.SafeArea(ft.Text("Hello, Flet!")))
+    page.title = "Charts and Tables"
 
+    def add_data(e):
+        table.rows.append(
+            ft.DataRow(
+                cells=[
+                    ft.DataCell(ft.Text(list_categories[categories.selected_index])),
+                    ft.DataCell(ft.Text(amount.value)),
+                ],
+            )
+        )
+        colors = [ft.colors.AMBER_200, ft.colors.TEAL_ACCENT_200, ft.colors.GREEN_ACCENT_200, ft.colors.DEEP_ORANGE_200]
+        chart.sections.append(
+            ft.PieChartSection(
+                value=amount.value,
+                title=list_categories[categories.selected_index],
+                title_position=2.0,
+                color=colors[categories.selected_index]
+            )
+        )
+        page.update()
+    
+
+    list_categories = ["Clothes", "Entertainment", "Food", "Transport"]
+    # Create categories for the chart
+    categories = ft.AutoComplete(
+        suggestions=[
+            ft.AutoCompleteSuggestion(value="Clothes"),
+            ft.AutoCompleteSuggestion(value="Entertainment"),
+            ft.AutoCompleteSuggestion(value="Food"),
+            ft.AutoCompleteSuggestion(value="Transport"),
+        ],
+        on_select=lambda e: print(e.control.selected_index, e.selection),
+    )
+    amount=ft.TextField(label="Amount: ", width=200)
+    add_btn = ft.ElevatedButton("Add", on_click=add_data)
+    table = ft.DataTable(
+        columns=[
+            # Identifier for each row
+            ft.DataColumn(ft.Text("Category")),
+            ft.DataColumn(ft.Text("Amount"))
+        ],
+        rows=[]
+    )
+    chart = ft.PieChart(
+        width=300,
+        height=300,
+        sections=[],    # Data will be added later
+        sections_space=1,
+        center_space_radius=0,
+        expand=True,
+    )
+
+    page.add(
+        ft.Row([ft.Column([categories], width=200), amount, add_btn]),
+        ft.Row([chart, table]),
+    )
+    page.update()
 
 ft.app(main)
